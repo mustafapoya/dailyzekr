1581146dbae614af2df174bbe5f73cc52b1c9463
MINE <mustafapoyaabc@gmail.com>
2019-12-01 14:23:22 +0430

    Adding MobAd, Home page add MobAd

--- app/src/main/java/com/ellia/dailyzekr/ui/home/HomeFragment.java
+++ app/src/main/java/com/ellia/dailyzekr/ui/home/HomeFragment.java
@@ -1,26 +1,33 @@
 package com.ellia.dailyzekr.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.ellia.dailyzekr.R;
 import com.ellia.dailyzekr.core.DailyZekrHandler;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private AdView mAdView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,7 +35,7 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        ImageView imgTodayZekr = root.findViewById(R.id.imgTodayZekr);
+        /*ImageView imgTodayZekr = root.findViewById(R.id.imgTodayZekr);
         imgTodayZekr.setImageResource(DailyZekrHandler.nameOfTheWeek());
         Button btnSetTodayZekr = root.findViewById(R.id.btnSetTodayZekr);
 
@@ -38,15 +45,20 @@ public class HomeFragment extends Fragment {
                 DailyZekrHandler.setTodayImage(view.getContext());
                 Toast.makeText(view.getContext(), "clicked", Toast.LENGTH_LONG).show();
             }
+        });*/
+
+        MobileAds.initialize(root.getContext(), new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
         });
 
-        /*final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
+        mAdView = root.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
         return root;
     }
+
 }
