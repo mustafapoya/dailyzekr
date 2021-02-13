5b89e573bdb565acfd7b62b1cf152c50e4b95364
MINE <mustafapoyaabc@gmail.com>
2021-02-13 23:05:33 +0430

    update daily zekr

--- app/src/main/java/com/ellia/dailyzekr/handlers/DailyZekrManager.java
+++ app/src/main/java/com/ellia/dailyzekr/handlers/DailyZekrManager.java
@@ -1,4 +1,105 @@
 package com.ellia.dailyzekr.handlers;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.ellia.dailyzekr.models.DailyZekr;
+import com.ellia.dailyzekr.models.Zekr;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class DailyZekrManager {
+    private Context context;
+    public DailyZekr zekr;
+    public final String TAG = "DailyZekrManager";
+
+    public DailyZekrManager(Context context) {
+        this.context = context;
+    }
+
+    public DailyZekr getDayZekr(int dayNumber) {
+        zekr = new DailyZekr();
+        JSONArray data = getJSonData();
+
+        try {
+            JSONObject jsonObject = data.getJSONObject(dayNumber);
+            zekr.setDay(jsonObject.getString("day"));
+            zekr.setZekr(jsonObject.getString("zekr"));
+            zekr.setEnglish(jsonObject.getString("trans_en"));
+            zekr.setFarsi(jsonObject.getString("trans_fa"));
+            zekr.setHindi(jsonObject.getString("trans_hi"));
+            zekr.setTurkey(jsonObject.getString("trans_tr"));
+
+            Log.d(TAG, "getDayZekr: " + zekr.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return zekr;
+    }
+
+    public ArrayList<DailyZekr> getAllDayZekrs() {
+        ArrayList<DailyZekr> zekrs = new ArrayList<>();
+        JSONArray data = getJSonData();
+
+        try {
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject jsonObject = data.getJSONObject(i);
+                DailyZekr zekr = new DailyZekr();
+                zekr.setDay(jsonObject.getString("day"));
+                zekr.setZekr(jsonObject.getString("zekr"));
+                zekr.setEnglish(jsonObject.getString("trans_en"));
+                zekr.setFarsi(jsonObject.getString("trans_fa"));
+                zekr.setHindi(jsonObject.getString("trans_hi"));
+                zekr.setTurkey(jsonObject.getString("trans_tr"));
+                zekrs.add(zekr);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return zekrs;
+    }
+
+    public int getRandomNumber() {
+        int high = getJSonData().length();
+        return new Random().nextInt(high - 1) + 1;
+    }
+
+    public JSONArray getJSonData() {
+        JSONArray allQuotes = null;
+
+        try {
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            allQuotes = obj.getJSONArray("daily_zekrs");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return allQuotes;
+    }
+
+    public String loadJSONFromAsset() {
+        String jsonData = null;
+
+        try {
+            InputStream is = context.getAssets().open("daily_zekrs.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            jsonData = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return jsonData;
+    }
 }
--- app/src/main/java/com/ellia/dailyzekr/ui/zekrs/ZekrsFragment.java
+++ app/src/main/java/com/ellia/dailyzekr/ui/zekrs/ZekrsFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,15 +17,22 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.ellia.dailyzekr.R;
+import com.ellia.dailyzekr.core.DailyZekrHandler;
+import com.ellia.dailyzekr.handlers.DailyZekrManager;
 import com.ellia.dailyzekr.handlers.QuotesManager;
 import com.ellia.dailyzekr.handlers.ZekrsManager;
+import com.ellia.dailyzekr.models.DailyZekr;
 import com.ellia.dailyzekr.models.Quote;
 import com.ellia.dailyzekr.models.Zekr;
 import com.ellia.dailyzekr.ui.zekrcounter.ZekrCounterViewModel;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ZekrsFragment extends Fragment {
     private ZekrCounterViewModel toolsViewModel;
@@ -38,6 +46,22 @@ public class ZekrsFragment extends Fragment {
         toolsViewModel = ViewModelProviders.of(this).get(ZekrCounterViewModel.class);
         View root = inflater.inflate(R.layout.fragment_zekrs, container, false);
         context = root.getContext();
+
+        TextView txtTodayZekr = (TextView)root.findViewById(R.id.txtTodayZekr);
+        TextView txtTodayZekrTrans = (TextView)root.findViewById(R.id.txtTodayZekrTrans);
+        TextView txtTodayName = (TextView)root.findViewById(R.id.txtTodayName);
+        TextView txtTodayDate = (TextView)root.findViewById(R.id.txtTodayDate);
+
+        DailyZekrManager dailyZekrsManager = new DailyZekrManager(context);
+        ArrayList<DailyZekr> dailyZekrs = dailyZekrsManager.getAllDayZekrs();
+
+
+        DailyZekr todayZekr = dailyZekrs.get(todayZekr()+6);
+        txtTodayZekr.setText(todayZekr.getZekr());
+        txtTodayZekrTrans.setText(todayZekr.getFarsi());
+        txtTodayName.setText(todayName());
+
+
 
         ZekrsManager zekrsManager = new ZekrsManager(context);
         ArrayList<Zekr> zekrs = zekrsManager.getAllZekrs();
@@ -65,5 +89,51 @@ public class ZekrsFragment extends Fragment {
         }
 
         return root;
+    }
+
+    public static int todayZekr() {
+        int dayOfTheWeek = DailyZekrHandler.getTodayName();
+
+        switch (dayOfTheWeek) {
+            case Calendar.SATURDAY:
+                return 0;
+            case Calendar.SUNDAY:
+                return 1;
+            case Calendar.MONDAY:
+                return 2;
+            case Calendar.TUESDAY:
+                return 3;
+            case Calendar.WEDNESDAY:
+                return 4;
+            case Calendar.THURSDAY:
+                return 5;
+            case Calendar.FRIDAY:
+                return 6;
+            default:
+                return 0;
+        }
+    }
+
+    public static String todayName() {
+        int dayOfTheWeek = DailyZekrHandler.getTodayName();
+
+        switch (dayOfTheWeek) {
+            case Calendar.SATURDAY:
+                return "شنبه";
+            case Calendar.SUNDAY:
+                return "یکشنبه";
+            case Calendar.MONDAY:
+                return "دوشنبه";
+            case Calendar.TUESDAY:
+                return "سه شنبه";
+            case Calendar.WEDNESDAY:
+                return "چهارشنبه";
+            case Calendar.THURSDAY:
+                return "پنجشنبه";
+            case Calendar.FRIDAY:
+                return "جمعه";
+            default:
+                return "نامشخص";
+        }
     }
 }
